b60269f8d8833d6c6bec19aecaeeaf0ccad5a3f5
Hanson <shadowguy369@gmail.com>
2021-12-24 17:37:48 -0800

    time stops at 999 now, didn't before

--- dist/timer.js
+++ dist/timer.js
@@ -9,15 +9,17 @@ const leftTime = document.querySelector("#red-num-timer-left");
 const midTime = document.querySelector("#red-num-timer-mid");
 const rightTime = document.querySelector("#red-num-timer-right");
 
-var time = 0;
+var time = 990;
 var delay = 1000;
 
+
 setInterval(function() {
-    time++;
-    if (time != 1000) {
-        rightTime.src = numbers[time%10];
-        midTime.src = numbers[Math.floor(time/10)%10]; 
-        leftTime.src = numbers[Math.floor(time/100)];
-    }
-    
+        time++;
+        if (!(time > 999)) {
+            rightTime.src = numbers[time%10];
+            midTime.src = numbers[Math.floor(time/10)%10]; 
+            leftTime.src = numbers[Math.floor(time/100)];
+        }
+     
 }, delay);
+

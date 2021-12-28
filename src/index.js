c0c61f831ad33ed0368e269c1c3e70d938adcb28
chrisp9974 <92195214+chrisp9974@users.noreply.github.com>
2021-12-27 16:18:14 -0800

    major changes: added generation for cover tiles, blank tiles are now set along with cover tiles
    beautifying changes: now switches cover tiles to blank tiles temporarily when mouse is held down over a cover tile, made border5 display as a table

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ const auth = getAuth();
 
 // Global variables
 var difficulty = '';
-
+var numClicks = 0;
 
 // Event listeners
 
@@ -43,6 +43,7 @@ const beginnerDiffBtn = document.querySelector('#diff-beg');
 beginnerDiffBtn.addEventListener('click', () => {
     difficulty = 'beginner';
     generateCoverTiles(difficulty);
+    numClicks = 0;
 });
 
 // Intermediate button
@@ -50,6 +51,7 @@ const intermediateDiffBtn = document.querySelector('#diff-int');
 intermediateDiffBtn.addEventListener('click', () => {
     difficulty = 'intermediate';
     generateCoverTiles(difficulty);
+    numClicks = 0;
 });
 
 // Expert button
@@ -57,34 +59,43 @@ const expertDiffBtn = document.querySelector('#diff-exp');
 expertDiffBtn.addEventListener('click', () => {
     difficulty = 'expert';
     generateCoverTiles(difficulty);
+    numClicks = 0;
 });
 
 // Creates the cover tiles
 // TODO: add custom difficulty option
 function generateCoverTiles(difficulty) {
+    // Show the board
+    document.querySelector('#border4').hidden = false;
+
     let boardWidth;
     let boardHeight;
+    let totMines;
     if (difficulty === 'beginner') {
         boardWidth = 9;
         boardHeight = 9;
+        totMines = 10;
     }
     else if (difficulty === 'intermediate') {
         boardWidth = 16;
         boardHeight = 16;
+        totMines = 40;
     }
     else if (difficulty === 'expert') {
         boardWidth = 30;
         boardHeight = 16;
+        totMines = 99;
     }
 
     cleanBoard();
-    createCoverTileRows(boardHeight, boardWidth);
-    setCoverTileProperties();
+    createBlankTiles(boardHeight, boardWidth);
+    setCoverTileProperties(totMines);
 }
 
 // Clears the board's rows
 function cleanBoard() {
-    const rows = document.querySelectorAll('.tile-row');
+    // Looks for all ids starting with (^=) "div-row"
+    const rows = document.querySelectorAll('[id^="div-row"]');
     const length = rows.length;
     console.log('removing ' + rows.length + ' rows');
     for (let i = 0; i < length; i++) {
@@ -92,30 +103,16 @@ function cleanBoard() {
     }
 }
 
-// Creates new divs and adds cover_block images to it
-function createCoverTileRows(boardHeight, boardWidth) {
-    for (let r = 0; r < boardHeight; r++) {
-        const border5 = document.querySelector('#border5');
-        const tileRowDiv = document.createElement('div');
-        tileRowDiv.classList.add('tile-row');
-        border5.appendChild(tileRowDiv);
-        for (let c = 0; c < boardWidth; c++) {
-            const coverTileImg = document.createElement('img');
-            coverTileImg.classList.add('cover-tile');
-            coverTileImg.src = '../images/cover_block.png';
-            tileRowDiv.appendChild(coverTileImg);
-        }
-    }
-}
-
-// Updates mine count and the alternates the image between cover_block and cover_block_flag
-function setCoverTileProperties() {
+// Properties of cover tiles
+function setCoverTileProperties(totMines) {
     const coverTiles = document.querySelectorAll('.cover-tile');
     for (let i = 0; i < coverTiles.length; i++) {
+
+        // Updates mine count and the alternates the image between cover_block and cover_block_flag
+        const imgSrc = coverTiles[i].src + '';
         coverTiles[i].addEventListener('contextmenu', (e) => {
             e.preventDefault();
 
-            const imgSrc = coverTiles[i].src + "";
             if (imgSrc.includes('flag')) {
                 coverTiles[i].src = '../images/cover_block.png';
                 numMines++;
@@ -126,20 +123,44 @@ function setCoverTileProperties() {
             }
             displayMineCount(numMines);
         });
+
+        // Whenever a cover tile is clicked, hide it
+        // First click code
+        coverTiles[i].addEventListener('click', () => {
+            numClicks++;
+            if (numClicks == 1) {
+                setMines(totMines, coverTiles[i]);
+                displayMineCount(totMines);
+            }
+            coverTiles[i].style.zIndex = -1;
+            console.log(coverTiles[i].id);
+        });
+
+        // When the mouse is held down and over a cover tile, switch it to a blank tile
+        // When the mouse is held down and leaves a cover tile, switch it back to a cover tile
+        let mousedown;
+        document.addEventListener('mousedown', () => {
+            mousedown = true;
+        });
+        document.addEventListener('mouseup', () => {
+            mousedown = false;
+        });
+
+        if (!imgSrc.includes('flag')) {
+            coverTiles[i].addEventListener('mouseover', () => {
+                if (mousedown == true) {
+                    coverTiles[i].src = '../images/tile_0.png';
+                }
+            });
+            coverTiles[i].addEventListener('mouseleave', () => {
+                if (mousedown == true) {
+                    coverTiles[i].src = '../images/cover_block.png';
+                }
+            });
+        }
     }
 }
 
-// TODO: input validation: numbers greater than 999
-let numMines = 0;
-const mineFormRef = document.querySelector('#user-mines-form');
-mineFormRef.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    numMines = mineFormRef.mines.value;
-    console.log(numMines);
-
-    displayMineCount(numMines);
-});
 
 // Changes the displayed flag counter
 // TODO: refactor this, probably can shorten it by a lot
@@ -191,6 +212,92 @@ function displayMineCount(numMines) {
 }
 
 
+// Sets a total of numMines anywhere except for the first click
+function setMines(numMines, firstClickCoverTile) {
+    // Gets all the blank tiles, converts it from a NodeList to an array
+    const allBlankTiles = Array.from(document.querySelectorAll('[id^="tile-row"]'));
+    
+    // Remove the first cover tile that was clicked from the array of possible mine locations
+    // substring(6) is used to remove 'cover-' from the id
+    const coverTileId = firstClickCoverTile.id + '';
+    const blankTileId = coverTileId.substring(6);
+    const blankTile = document.querySelector('#' + blankTileId);
+    allBlankTiles.splice(allBlankTiles.indexOf(blankTile), 1);
+    console.log(allBlankTiles);
+
+    let maxRandInt = allBlankTiles.length - 1;
+    for (let i = 0; i < numMines; i++) {
+        // Generate a random index of the allBlankTiles array to be a mine
+        const randomInt = Math.floor(Math.random() * maxRandInt);
+        const mine = allBlankTiles[randomInt];
+
+        // Take out the blank tile from the array and decrease the max value that can be randomly generated
+        allBlankTiles.splice(randomInt, 1);
+        maxRandInt--;
+
+        // Change the tile's id to indicate that it is a mine
+        // Id: mine-tile-row-#-col-#
+        mine.id = 'mine-' + mine.id;
+        mine.src = '../images/mine.png';
+    }
+}
+
+// Set blank tiles and cover tiles
+function createBlankTiles(boardHeight, boardWidth) {
+    for (let r = 0; r < boardHeight; r++) {
+        const tileRowDiv = createTableRowDiv(r);
+        for (let c = 0; c < boardWidth; c++) {
+            const blankAndCoverTilePair = createTilePairDiv(tileRowDiv, r, c);
+            createBlankTile(blankAndCoverTilePair, r, c);
+            createCoverTile(blankAndCoverTilePair, r, c);
+        }
+    }
+}
+
+// Creates div that acts as a table row
+// Returns the created div
+// Id: div-row-#
+function createTableRowDiv(r) {
+    const border5 = document.querySelector('#border5');
+    const tileRowDiv = document.createElement('div');
+    tileRowDiv.style.display = 'table-row';
+    tileRowDiv.id = 'div-row-' + r + '';
+    border5.appendChild(tileRowDiv);
+    return tileRowDiv;
+}
+
+// Creates div that holds the cover and blank tile pair and adds it to the div in the param
+// Returns the created div
+// Id: pair-row-#-col-#
+function createTilePairDiv(tileRowDiv, r, c) {
+    const blankAndCoverTilePair = document.createElement('div');
+    blankAndCoverTilePair.classList.add('pair-row-' + r + '-col-' + c + '');
+    blankAndCoverTilePair.style.display = 'table-cell';
+    tileRowDiv.appendChild(blankAndCoverTilePair);
+    return blankAndCoverTilePair;
+}
+
+// Creates a blank tile and adds it to the div in the param
+// Id: tile-row-#-col-#
+function createBlankTile(blankAndCoverTilePair, r, c) {
+    const blankTile = document.createElement('img');
+    blankTile.classList.add('tile');
+    blankTile.id = 'tile-row-' + r + '-col-' + c + '';
+    blankTile.src = '../images/tile_0.png';
+    blankTile.setAttribute('draggable', false);
+    blankAndCoverTilePair.appendChild(blankTile);
+}
+
+// Creates a cover tile and adds it to the div in the param
+// Id: cover-tile-row-#-col-#
+function createCoverTile(blankAndCoverTilePair, r, c) {
+    const coverTile = document.createElement('img');
+    coverTile.classList.add('cover-tile');
+    coverTile.id = 'cover-tile-row-' + r + '-col-' + c + '';
+    coverTile.src = '../images/cover_block.png';
+    coverTile.setAttribute('draggable', false);
+    blankAndCoverTilePair.appendChild(coverTile);
+}
 
 // Code for the timer
 
@@ -223,7 +330,7 @@ function updateDisplay(time) {
     for (let i = 0; i <= 9; i++) {
         numbers[i] = "../images/red_" + i + ".png";
     }
-    
+
     const leftTime = document.querySelector("#red-num-timer-left");
     const midTime = document.querySelector("#red-num-timer-mid");
     const rightTime = document.querySelector("#red-num-timer-right");
@@ -235,7 +342,8 @@ function updateDisplay(time) {
     }
 }
 
-// Event listeners
+
+// Code for testing purposes
 const startTimeBtn = document.querySelector('#start-timer');
 startTimeBtn.addEventListener('click', () => {
     startTimer();
@@ -245,4 +353,25 @@ const stopTimeBtn = document.querySelector('#stop-timer');
 stopTimeBtn.addEventListener('click', () => {
     const endTime = stopTimer();
     console.log(endTime);
+});
+
+const showMinesBtn = document.querySelector('#show-mines');
+showMinesBtn.addEventListener('click', () => {
+    const allBlanks = document.querySelectorAll('[id^="mine"]');
+    for (let i = 0; i < allBlanks.length; i++) {
+        allBlanks[i].style.zIndex = 5;
+    }
+    console.log(allBlanks.length);
+});
+
+// TODO: input validation: numbers greater than 999
+let numMines = 0;
+const mineFormRef = document.querySelector('#user-mines-form');
+mineFormRef.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    numMines = mineFormRef.mines.value;
+    console.log(numMines);
+
+    displayMineCount(numMines);
 });
